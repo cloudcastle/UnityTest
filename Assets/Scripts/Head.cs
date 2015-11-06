409ef7f1091e10529465285edc4a8aa93eb0ae4d
craus <craus902@gmail.com>
2015-11-06 09:32:46 +0400

    initial commit of unity project - room, floor, 1 wall, move, jump, rotate body/head, light

---
+++ Assets/Scripts/Head.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Head : MonoBehaviour
+{
+    public float verticalRotateSpeed = 6.0F;
+
+    void Update()
+    {
+        transform.RotateAround(transform.position, transform.right, -Input.GetAxis("Mouse Y") * verticalRotateSpeed);
+    }
+}
---
+++ Assets/Scripts/KeyboardRotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardRotator : MonoBehaviour {
+    float rotationSpeed = 100;
+
+	void Update () {
+        transform.Rotate(Vector3.up, Time.deltaTime * Input.GetAxis("Horizontal") * rotationSpeed);
+	}
+}
