589b5af59226b5e34befbe9d99b6b567f0c24414
craus <craus902@gmail.com>
2015-11-07 04:42:51 +0400

    bricks, crouch, text serialization

---
+++ Assets/Scripts/Crouch.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class Crouch : MonoBehaviour
+{
+    public float crouchSpeed = 8.0F;
+    public float uncrouchSpeed = 8.0F;
+    public float maxHeight = 2;
+    public float minHeight = 0;
+    float currentHeight = 2;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    CharacterController characterController;
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (PauseManager.paused)
+        {
+            return;
+        }
+        if (Input.GetButton("Crouch"))
+        {
+            currentHeight = Mathf.Max(currentHeight - crouchSpeed * Time.deltaTime, minHeight);
+        }
+        else
+        {
+            currentHeight = Mathf.Min(currentHeight + uncrouchSpeed * Time.deltaTime, maxHeight);
+        }
+        characterController.height = currentHeight;
+    }
+}
---
+++ Assets/Scripts/DisableShortcutsInEditor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class DisableShortcutsInEditor : MonoBehaviour
+{
+    static void blocked(string item)
+    {
+        Debug.Log(item);
+        if (!Application.isPlaying)
+        {
+            EditorApplication.ExecuteMenuItem(item);
+        }
+    }
+
+    [MenuItem("Disabled in Play Mode/Scene %1")]
+    static void WindowScene()
+    {
+        blocked("Window/Scene");
+    }
+
+    [MenuItem("Disabled in Play Mode/Save Scene %s")]
+    static void SaveScene()
+    {
+        blocked("File/Save Scene");
+    }
+
+    [MenuItem("Disabled in Play Mode/Select All %a")]
+    static void SelectAll()
+    {
+        blocked("Edit/Select All");
+    }
+
+    [MenuItem("Disabled in Play Mode/Duplicate %d")]
+    static void Duplicate()
+    {
+        blocked("Edit/Duplicate");
+    }
+
+    void OnGuI()
+    {
+        Event.current.Use();
+    }
+}
