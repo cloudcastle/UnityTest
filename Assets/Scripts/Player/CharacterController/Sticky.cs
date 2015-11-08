5ba3f79b99113e25a743e71f98bc54dac3216532
craus <craus902@gmail.com>
2015-11-09 00:45:24 +0400

    attempt to fix Sticky behaviour big ground and building markup

--- Assets/Scripts/Player/CharacterController/Sticky.cs
+++ Assets/Scripts/Player/CharacterController/Sticky.cs
@@ -4,6 +4,11 @@ using System.Collections;
 [RequireComponent(typeof(CharacterController))]
 public class Sticky : MonoBehaviour
 {
+    /// <summary>
+    /// Some object in local system at the global distance of 1 from unit's origin
+    /// </summary>
+    public Transform sample;
+
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
         if (hit.normal.y > 0.9f) 
@@ -11,9 +16,25 @@ public class Sticky : MonoBehaviour
             if (hit.transform != transform.parent)
             {
                 transform.SetParent(hit.transform, true);
+                //RestoreScale();
+
                 Debug.Log("Parented to " + hit.gameObject.Path());
                 Debug.Log("Global scale is now " + transform.lossyScale);
+                Debug.Log("Sample distance is now " + SampleDistance());
+                Debug.Log("Height is now " + GetComponent<CharacterController>().height);
+                Debug.Log("Radius is now " + SampleDistance());
             }
         }
+    }
+
+    float SampleDistance()
+    {
+        return (transform.position - sample.transform.position).magnitude;
+    }
+
+    void RestoreScale()
+    {
+        transform.localScale = Vector3.one;
+        transform.localScale = Vector3.one / SampleDistance();
     }
 }
