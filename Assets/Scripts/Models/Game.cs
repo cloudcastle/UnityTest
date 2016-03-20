69fd7bc4118241630dd3463b9d08c58e34530419
craus <craus902@gmail.com>
2016-03-21 01:57:27 +0400

    ascention level

--- Assets/Scripts/Models/Game.cs
+++ Assets/Scripts/Models/Game.cs
@@ -11,24 +11,32 @@ public class Game
         var mouseMove = new Level("MouseMove", depends: click);
         var wasd = new Level("WASD", depends: mouseMove);
         var space = new Level("Space", depends: wasd);
+
         var raise = new Level("Raise", depends: wasd);
         var order = new Level("Order", depends: raise);
         var precaution = new Level("Precaution", depends: order);
+
         var stairs = new Level("Stairs", depends: precaution);
         var twoPits = new Level("Two pits", depends: precaution);
         var hold = new Level("Hold", depends: precaution);
+
+        var ascention = new Level("Ascention", depends: precaution);
      
         levels = new List<Level>() {
             click,
             mouseMove,
             wasd,
             space,
+
             raise,
             order,
             precaution,
+
             stairs,
             twoPits,
             hold,
+
+            ascention,
         };
     }
 }
