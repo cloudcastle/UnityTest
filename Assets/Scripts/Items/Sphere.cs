937ca31ad606b2058ad37bea9927c2c3b935acee
craus <craus902@gmail.com>
2016-03-24 22:33:14 +0400

    CRITICAL: fix bug with leaking materials

--- Assets/Scripts/Items/Sphere.cs
+++ Assets/Scripts/Items/Sphere.cs
@@ -16,10 +16,16 @@ public class Sphere : Item
         if (material == null) {
             material = meshRenderer.sharedMaterial;
         }
-        if (meshRenderer.sharedMaterial == null || meshRenderer.sharedMaterial.color != color || meshRenderer.sharedMaterial.name != material.name) {
-            var tempMaterial = new Material(material);
-            tempMaterial.color = color;
-            meshRenderer.sharedMaterial = tempMaterial;
+        if (meshRenderer.sharedMaterial == null || meshRenderer.sharedMaterial.color != color) {
+            Debug.Log("Sphere: material update");
+            UpdateRendererMaterial();
         }
+    }
+
+    [ContextMenu("Update renderer material")]
+    void UpdateRendererMaterial() {
+        var tempMaterial = new Material(material);
+        tempMaterial.color = color;
+        meshRenderer.sharedMaterial = tempMaterial;
     }
 }
