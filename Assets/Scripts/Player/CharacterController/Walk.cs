1462df447943fd64d92b4cc69ccafe5098e3ea63
craus <craus902@gmail.com>
2016-04-02 00:23:23 +0400

    more issues; fixed fastrun

--- Assets/Scripts/Player/CharacterController/Walk.cs
+++ Assets/Scripts/Player/CharacterController/Walk.cs
@@ -9,11 +9,17 @@ public class Walk : MonoBehaviour
 {
     public float speed = 6;
 
+    public float currentSpeed;
+
     void Awake() {
         GetComponent<Move>().additionalVelocities.Add(() => {
             var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             move = transform.TransformDirection(move);
+            if (move.magnitude > 1) {
+                move.Normalize();
+            }
             move *= speed;
+            currentSpeed = move.magnitude;
             move.y = 0;
             return move;
         });
