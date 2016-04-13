eb844cdbd0384d17479337c859ed159957e63117
craus <craus902@gmail.com>
2016-04-14 02:45:21 +0400

    level graph: superzoom and drag for camera controls

--- Assets/Scripts/Level Graph/CameraControl.cs
+++ Assets/Scripts/Level Graph/CameraControl.cs
@@ -3,6 +3,7 @@
 public class CameraControl : MonoBehaviour
 {
     float zoomSpeed = 1.1f;
+    Vector3 draggingWorldPoint;
 
     new Camera camera;
 
@@ -10,12 +11,26 @@ public class CameraControl : MonoBehaviour
         camera = GetComponent<Camera>();
     }
 
+    void zoom(float times) {
+        var mouseWorldPoint = camera.ScreenToWorldPoint(Input.mousePosition);
+        var radiusVector = mouseWorldPoint - transform.position;
+        transform.Translate(radiusVector * (1 - times));
+        camera.orthographicSize *= times;
+    }
+
     void Update() {
         if (Input.GetAxisRaw("Mouse ScrollWheel") > 0) {
-            camera.orthographicSize /= zoomSpeed;
+            zoom(1f / zoomSpeed);
         }
         if (Input.GetAxisRaw("Mouse ScrollWheel") < 0) {
-            camera.orthographicSize *= zoomSpeed;
+            zoom(zoomSpeed);
+        }
+        if (Input.GetMouseButtonDown(1)) {
+            draggingWorldPoint = camera.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if (Input.GetMouseButton(1)) {
+            var mouseWorldPoint = camera.ScreenToWorldPoint(Input.mousePosition);
+            transform.Translate(draggingWorldPoint - mouseWorldPoint);
         }
     }
 }
