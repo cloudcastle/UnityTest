b2bfe4bbca487e186cd5d7517f412c43c75ae816
craus <craus902@gmail.com>
2016-04-14 02:28:55 +0400

    camera controls

---
+++ Assets/Scripts/Level Graph/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    float zoomSpeed = 1.1f;
+
+    new Camera camera;
+
+    void OnEnable() {
+        camera = GetComponent<Camera>();
+    }
+
+    void Update() {
+        if (Input.GetAxisRaw("Mouse ScrollWheel") > 0) {
+            camera.orthographicSize /= zoomSpeed;
+        }
+        if (Input.GetAxisRaw("Mouse ScrollWheel") < 0) {
+            camera.orthographicSize *= zoomSpeed;
+        }
+    }
+}
