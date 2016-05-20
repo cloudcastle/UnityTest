dff996ec190f921e69a4c79943df29c2b4fd9ea6
craus <craus902@gmail.com>
2016-05-21 02:07:41 +0400

    timestop affects items

---
+++ Assets/Scripts/Activatables/Items/TimeStoppable.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TimeStoppable : MonoBehaviour
+{
+    Vector3 savedVelocity;
+    Vector3 savedAngularVelocity;
+    Rigidbody rigidBody;
+
+    bool timeStopped = false;
+
+    void Awake() {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        if (!timeStopped && TimeManager.timestopped) {
+            savedVelocity = rigidBody.velocity;
+            savedAngularVelocity = rigidBody.angularVelocity;
+            rigidBody.velocity = Vector3.zero;
+            rigidBody.angularVelocity = Vector3.zero;
+            rigidBody.useGravity = false;
+            timeStopped = true;
+        }
+        if (TimeManager.timestopped) {
+            if (rigidBody.velocity != Vector3.zero) {
+                savedVelocity = rigidBody.velocity;
+            }
+            if (rigidBody.angularVelocity != Vector3.zero) {
+                savedAngularVelocity = rigidBody.angularVelocity;
+            }
+            rigidBody.velocity = Vector3.zero;
+            rigidBody.angularVelocity = Vector3.zero;
+        }
+        if (timeStopped && !TimeManager.timestopped) {
+            rigidBody.velocity = savedVelocity;
+            rigidBody.angularVelocity = savedAngularVelocity;
+            rigidBody.useGravity = true;
+            timeStopped = false;
+        }
+    }
+}
