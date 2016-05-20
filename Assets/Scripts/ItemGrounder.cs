a8246ed40ce41f497b9f50f54ef1ec626796e428
craus <craus902@gmail.com>
2016-05-21 01:36:50 +0400

    attempt to carry items on moving surfaces

---
+++ Assets/Scripts/ItemGrounder.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class ItemGrounder : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    void Awake() {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionStay(Collision c)
+    {
+        if (!enabled) {
+            return;
+        }
+        if (TimeManager.Paused) {
+            return;
+        }
+        var movingSurface = c.gameObject.GetComponent<MovingSurface>();
+        if (movingSurface != null) {
+            var relativeVelocity = movingSurface.currentVelocity - rigidBody.velocity;
+            //Debug.LogFormat("Relative velocity: {0}", relativeVelocity);
+            rigidBody.AddForce(relativeVelocity, ForceMode.VelocityChange);
+        }
+    }
+}
