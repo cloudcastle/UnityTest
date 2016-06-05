06c0bc1e1cf7c3e80dea80855037080356f75fa4
craus <craus902@gmail.com>
2016-06-05 13:44:19 +0400

    reworked ghost for items: ghost until leaves thrower

--- Assets/Scripts/Activatables/Items/Item.cs
+++ Assets/Scripts/Activatables/Items/Item.cs
@@ -10,9 +10,18 @@ public class Item : Activatable
     public event Action<Unit> onPick = (p) => { };
     public event Action<Unit> onLose = (p) => { };
 
+    bool ghostForm = false;
+    Unit thrower = null;
+
     public override void Start() {
         base.Start();
         new ValueTracker<InventorySlot>(v => inventorySlot = v, () => inventorySlot);
+        new ValueTracker<bool>(v => {
+            ghostForm = v;
+            SetSemitransparent(ghostForm);
+            Extensions.IgnoreCollision(thrower, this, false);
+        }, () => ghostForm);
+        new ValueTracker<Unit>(v => thrower = v, () => thrower);
     }
 
     public override void Activate(Activator activator) {
@@ -39,18 +48,45 @@ public class Item : Activatable
     public virtual void SetSemitransparent(bool on = true) {
     }
 
+    bool OverlappingWithThrower() {
+        SpaceScanner.overlapCount = Physics.OverlapSphereNonAlloc(
+            transform.position,
+            GetComponent<SphereCollider>().radius * transform.lossyScale.x,
+            SpaceScanner.overlapResults,
+            LayerMask.GetMask("Unit")
+        );
+        for (int i = 0; i < SpaceScanner.overlapCount; i++) {
+            var unit = SpaceScanner.overlapResults[i].gameObject.GetComponent<Unit>();
+            if (unit == thrower) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void FixedUpdate() {
+        if (ghostForm) {
+            if (!OverlappingWithThrower()) {
+                GhostFormOff();
+            }
+        }
+    }
+
+    void GhostFormOff() {
+        Extensions.IgnoreCollision(thrower, this, false);
+        SetSemitransparent(false);
+        if (DebugManager.debug) {
+            Debug.Log(String.Format("Item {0} unghost for {1}", this, thrower));
+        }
+    }
+
     public void GhostFor(Unit player) {
         if (DebugManager.debug) {
             Debug.Log(String.Format("Item {0} ghost for {1}", this, player));
         }
         Extensions.IgnoreCollision(player, this);
         SetSemitransparent();
-        TimeManager.WaitForStoppable(ghostTimeAfterThrow).Then(() => {
-            Extensions.IgnoreCollision(player, this, false);
-            SetSemitransparent(false);
-            if (DebugManager.debug) {
-                Debug.Log(String.Format("Item {0} unghost for {1}", this, player));
-            }
-        });
+        ghostForm = true;
+        thrower = player;
     }
 }
---
+++ Assets/Scripts/Common/SpaceScanner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+public static class SpaceScanner
+{
+    const int MAX_RESULTS_COUNT = 1000;
+    public static int overlapCount;
+    public static Collider[] overlapResults = new Collider[MAX_RESULTS_COUNT];
+}
