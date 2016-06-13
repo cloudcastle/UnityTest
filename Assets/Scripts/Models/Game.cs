80fe733af572bb4ffdb8e4cfc0d38eae64c32549
craus <craus902@gmail.com>
2016-06-13 06:52:49 +0400

    2 Cor 11 33 level

--- Assets/Scripts/Models/Game.cs
+++ Assets/Scripts/Models/Game.cs
@@ -182,6 +182,7 @@ public class Game
 
         // Timestop again
         var claws = new Level("Claws", support);
+        var _2cor11_33 = new Level("2 Cor 11 33", support, gift, greenCabin);
 
         // Buttons 
         var closer = new Level("Closer", close, giveALift, v);
