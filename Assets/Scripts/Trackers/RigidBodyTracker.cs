d08d374fb085c637d5103b3722583bf7c5e2a541
craus <pavel.semushin@cloudcastlegroup.com>
2017-05-25 04:02:03 +0400

    keep awake option for rigid body tracker

--- Assets/Scripts/Trackers/RigidBodyTracker.cs
+++ Assets/Scripts/Trackers/RigidBodyTracker.cs
@@ -8,8 +8,11 @@ public class RigidBodyTracker : MonoBehaviour
 {
     public new Rigidbody rigidbody;
 
+    public bool keepWakeUp = false;
+
     void Awake() {
         rigidbody = GetComponent<Rigidbody>();
+        //rigidbody.neve
     }
 
     public Vector3Tracker velocityTracker;
@@ -20,5 +23,18 @@ public class RigidBodyTracker : MonoBehaviour
         velocityTracker = new Vector3Tracker((v) => rigidbody.velocity = v, () => rigidbody.velocity);
         angularVelocityTracker = new Vector3Tracker((v) => rigidbody.angularVelocity = v, () => rigidbody.angularVelocity);
         isKinematicTracker = new BoolTracker((v) => rigidbody.isKinematic = v, () => rigidbody.isKinematic);
+//        new BoolTracker((v) => {
+//            if (v) {
+//                rigidbody.Sleep();
+//            } else {
+//                rigidbody.WakeUp();
+//            }
+//        }, () => rigidbody.IsSleeping());
+    }
+
+    void FixedUpdate() {
+        if (keepWakeUp) {
+            rigidbody.WakeUp();
+        }
     }
 }
