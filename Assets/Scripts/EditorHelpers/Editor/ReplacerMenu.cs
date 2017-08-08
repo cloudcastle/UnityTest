accd968b2fcd146504838dfab097f6d91b5b657d
Pavel Semushin <pavel.semushin@castle.co>
2017-08-08 13:53:26 +0400

    replace jumpers to prefabs on some levels

---
+++ Assets/Scripts/EditorHelpers/Editor/ReplacerMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using UnityEditor;
+
+public class ReplacerMenu : Editor
+{
+    [MenuItem("Custom/Replace To Prefabs/Jumpers")]
+    private static void ReplaceJumpersToPrefabs() {
+        FindObjectOfType<ReplaceToPrefabs>().Replace();
+    }
+}
+
+
---
+++ Assets/Scripts/EditorHelpers/ReplaceToPrefabs.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class ReplaceToPrefabs : MonoBehaviour
+{
+    public Jumper jumperPrefab;
+
+    [ContextMenu("Replace to prefabs")]
+    public void Replace() {
+//        var jumper = PrefabUtility.InstantiatePrefab(jumperPrefab) as Jumper;
+//        jumper.transform.position = new Vector3(-14, 0.1f, 22);
+//        //Instantiate(jumperPrefab, new Vector3(-14, 0.1f, 22), Quaternion.identity);
+//        EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
+
+        FindObjectsOfType<Jumper>().ForEach(j => {
+            if (PrefabUtility.GetPrefabParent(j) == null) {
+                Debug.LogFormat("Replacing jumper at {0} to prefab", j.transform.position);
+                var jumper = PrefabUtility.InstantiatePrefab(jumperPrefab) as Jumper;
+                jumper.transform.SetParent(j.transform.parent);
+                jumper.transform.position = j.transform.position;
+                jumper.transform.rotation = j.transform.rotation;
+                DestroyImmediate(j);
+            }
+        });
+        Debug.LogFormat("Done");
+    }
+}
+
+#endif
+
