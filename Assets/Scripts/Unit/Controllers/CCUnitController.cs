0f3932dc62ca4cb2849fe05a2ec2418b688aaed3
Pavel Semushin <pavel.semushin@castle.co>
2017-09-21 14:02:43 +0400

    fix CCUnitController to give correct grounded info after undoing; updated issues list

--- Assets/Scripts/Unit/Controllers/CCUnitController.cs
+++ Assets/Scripts/Unit/Controllers/CCUnitController.cs
@@ -9,15 +9,24 @@ public class CCUnitController : UnitGeometryController
 {
     CharacterController characterController;
 
+    bool isGroundedValid = true;
+
     public void Awake() {
         characterController = GetComponent<CharacterController>();
     }
 
+    public void Start() {
+        TimeManager.instance.onUndo += () => {
+            isGroundedValid = false;
+        };
+    }
+
     public override bool IsGrounded() {
-        return characterController.isGrounded;
+        return characterController.isGrounded && isGroundedValid;
     }
 
     public override void SetVelocity(Vector3 v) {
+        isGroundedValid = true;
         characterController.Move(v * Time.fixedDeltaTime);
     }
 }
