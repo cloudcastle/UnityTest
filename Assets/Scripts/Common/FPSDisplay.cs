0756252b3775b1c8d75fc77a06edf9e0417b911f
Pavel Semushin <pavel.semushin@castle.co>
2017-09-21 17:14:14 +0400

    added inertia back to characters, but fixed and clear (possibly)

--- Assets/Scripts/Common/FPSDisplay.cs
+++ Assets/Scripts/Common/FPSDisplay.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class FPSDisplay : MonoBehaviour
 {
     float deltaTime = 0.0f;
+
+    List<float> times = new List<float>();
 
     void Update()
     {
@@ -22,7 +26,12 @@ public class FPSDisplay : MonoBehaviour
         style.normal.textColor = new Color (0.0f, 0.0f, 0.5f, 1.0f);
         float msec = deltaTime * 1000.0f;
         float fps = 1.0f / deltaTime;
-        string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+        if (times.Count > 1000) {
+            times.Clear();
+        }
+        times.Add(msec);
+        float maxtime = times.Max();
+        string text = string.Format("{0:0.0} ms ({1:0.} fps) [max {2:0.0} ms]", msec, fps, maxtime);
         GUI.Label(rect, text, style);
     }
 }
--- Assets/Scripts/Unit/CharacterController/Walk.cs
+++ Assets/Scripts/Unit/CharacterController/Walk.cs
@@ -14,21 +14,37 @@ public class Walk : Ability
 
     public float currentSpeed;
 
+    public Vector3 currentVelocity;
+
+    /// <summary>
+    /// Inertial velocity delta part left after each 1 second
+    /// </summary>
+    public float inertia = 0.01f;
+
     public override void Awake() {
         base.Awake();
         walkTransform = transform;
         GetComponent<Move>().additionalVelocities.Add(() => {
-            var move = new Vector3(Controller.Move().x, 0, Controller.Move().y);
-            move = walkTransform.TransformDirection(move);
-            if (move.magnitude > 1) {
-                move.Normalize();
-            }
-            move *= speed;
+            var move = currentVelocity;
+
             currentSpeed = move.magnitude;
             if (zeroY) {
                 move.y = 0;
             }
             return move;
         });
+    }
+
+    public void FixedUpdate() {
+        var move = new Vector3(Controller.Move().x, 0, Controller.Move().y);
+        move = walkTransform.TransformDirection(move);
+        if (move.magnitude > 1) {
+            move.Normalize();
+        }
+        move *= speed;
+        currentVelocity = Vector3.Lerp(currentVelocity, move, 1-Mathf.Pow(inertia, TimeManager.FixedDeltaTime));
+        if (Player.instance.current != this.unit) {
+            currentVelocity = Vector3.zero;
+        }
     }
 }
